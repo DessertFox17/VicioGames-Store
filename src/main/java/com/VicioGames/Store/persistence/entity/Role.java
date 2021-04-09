3d6ccf18e84749f0955827203ad6a50334341bc1
Juan Arango <jblackheart581@gmail.com>
2021-04-09 01:53:07 -0500

    Role entity created

---
+++ src/main/java/com/VicioGames/Store/persistence/entity/Role.java
@@ -0,0 +1,31 @@
+package com.VicioGames.Store.persistence.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Integer roleId;
+
+    @Column(name = "r_name")
+    private String rName;
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getrName() {
+        return rName;
+    }
+
+    public void setrName(String rName) {
+        this.rName = rName;
+    }
+}
